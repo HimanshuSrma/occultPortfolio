e157b29a2ee2a703a7b04ed525827f6c81ab66b3
Himanshu Sharma <himanshu1@wattmonk.com>
2025-09-23 15:01:08 +0530

    # ind fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, OnInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import e from 'express';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,23 @@ import { HeaderComponent } from './components/header/header.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  async ngAfterViewInit() {
+export class AppComponent {
+
+  // ngOnInit() {
+  //   try {
+  //     let isDevPortfolioVisitor = window.localStorage.getItem('isDevPortfolioVisitor');
+  //     if(isDevPortfolioVisitor && window.location.hostname != 'localhost'){
+  //       window.location.href = 'https://dev-portfolio.vercel.app';
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+      
+  //   }
+  //   console.log('AppComponent initialized');
+  // }
+  // async ngAfterViewInit() {
     // keep for dynamic remote attach if needed later
     // const RemoteCmp = await loadRemoteModule({...}).then(m => m.AppComponent);
     // this.viewContainerRef.createComponent(RemoteCmp);
-  }
+  // }
 }
